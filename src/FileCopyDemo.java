06add830d4edbddcda68b0c176549d8d80d2c629
AE206_PC6 <AE206_PC6@Infva06186>
2018-08-03 18:21:53 +0530

    Third

---
+++ src/FileCopyDemo.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class FileCopyDemo {
+
+	public static void main(String[] args) throws Exception{
+		FileInputStream istream = new FileInputStream("D:/Demo/vsc.exe");
+		FileOutputStream ostream = new FileOutputStream("D:/Demo/vsc2.exe");
+		long startTime = System.currentTimeMillis();
+		int ch =0 ;
+		while(true) {
+			ch= istream.read();
+			if (ch==-1) break;
+			ostream.write(ch);
+		}
+	System.out.println("Time: "+(System.currentTimeMillis()- startTime));
+	ostream.close();
+	istream.close();
+	}
+}
